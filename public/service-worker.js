d3e95468ad7cadf6b7d9979e370f9fdcaaa054b9
Your Name <you@example.com>
2022-08-28 15:37:20 -0400

    Activate service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,11 +9,33 @@ const FILES_TO_CACHE = [
     "./js/idb.js",
 ];
 
+// install service worker, add files to precache
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
           console.log('installing cache : ' + CACHE_NAME)
           return cache.addAll(FILES_TO_CACHE)
         })
-    )
-})
+    );
+});
+
+// activate service worker, clear any old data from the cache and tell the service worker how to manage caches
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+      caches.keys().then(function(keyList) {
+        let cacheKeeplist = keyList.filter(function(key) {
+          return key.indexOf(APP_PREFIX);
+        });
+        cacheKeeplist.push(CACHE_NAME);
+
+        return Promise.all(
+          keyList.map(function(key, i) {
+            if (cacheKeeplist.indexOf(key) === -1) {
+              console.log('deleting cache : ' + keyList[i]);
+              return caches.delete(keyList[i]);
+            }
+          })
+        );
+      })
+    );
+});
